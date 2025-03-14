e03a3d2a6820a6aad84d5a7d8696a133c82db23c
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-14 23:12:17 +0500

    make a logic to add all natural number to specific condition

--- Recursion.java
+++ Recursion.java
@@ -11,10 +11,20 @@ public class Recursion {
           
         
     }
+    // print natural number
+    public static void natural(int i,int n,int sum){
+        if(i > n){
+            System.out.println(sum);
+            return;
+        } 
+        sum = sum + i;
+        natural(i + 1, n, sum);
+    }
 
     public static void main(String[] args) {
         int n = 5;
         factorial(n);
         
-    }
-}
+        
+        natural(1, 5, 0);
+}}
