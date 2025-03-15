4fb14b791f2a45b8223c972de35b40365c0159cb
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-15 05:53:51 +0500

    make a logic of find x^n by usung recursive method

--- Recursion.java
+++ Recursion.java
@@ -20,6 +20,7 @@ public class Recursion {
         sum = sum + i;
         natural(i + 1, n, sum);
     }
+    //print fabbonaci series
 public static void fabonacci(int a, int b, int f) {
     if (f == 0) {
         return;
@@ -28,12 +29,31 @@ public static void fabonacci(int a, int b, int f) {
     System.out.println(c);
     fabonacci(b, c, f - 1); // Decrement f by 1 for correct recursion
 }
-    public static void main(String[] args) {
+//print stack till x^n
+public static int calcpower(int x ,int p){
+    if(p==0){
+        return 1;
+    }
+    if (x==0) {
+        return 0;
+    }
+    int Xn= calcpower(x, p-1);
+    int powe = x*Xn;
+    return powe;
+
+}
+public static void main(String[] args) {
         int n = 5;
         printNum(n);
         natural(1, 5, 0);
         int f=7;
-        fabonacci(0,1,f);
+       
         fabonacci(0, 1, f);
+        int ans=calcpower(2,5);
+        System.out.println(ans);
             }
+           
+            
         }
+        
+
